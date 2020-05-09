b774bc17f127ca05173c0ae018065a3a37377645
Oliver Lew <oliver_lew@outlook.com>
2020-05-09 22:23:18 +0800

    Apply patch sticky, additionally added to rules
    The sticky rule might be useful for e.g. PictureInPicture mode of Firefox

--- config.def.h
+++ config.def.h
@@ -26,9 +26,9 @@ static const Rule rules[] = {
 	 *	WM_CLASS(STRING) = instance, class
 	 *	WM_NAME(STRING) = title
 	 */
-	/* class      instance    title       tags mask     iscentered   isfloating   monitor */
-	{ "Gimp",     NULL,       NULL,       0,            0,           1,           -1 },
-	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           -1 },
+	/* class      instance    title       tags mask     iscentered   isfloating   issticky   monitor */
+	{ "Gimp",     NULL,       NULL,       0,            0,           1,           0,         -1 },
+	{ "Firefox",  NULL,       NULL,       1 << 8,       0,           0,           0,         -1 },
 };
 
 /* layout(s) */
@@ -79,6 +79,7 @@ static Key keys[] = {
 	{ MODKEY,                       XK_space,  setlayout,      {0} },
 	{ MODKEY|ShiftMask,             XK_space,  togglefloating, {0} },
 	{ MODKEY|ShiftMask,             XK_f,      togglefullscr,  {0} },
+	{ MODKEY,                       XK_s,      togglesticky,   {0} },
 	{ MODKEY,                       XK_0,      view,           {.ui = ~0 } },
 	{ MODKEY|ShiftMask,             XK_0,      tag,            {.ui = ~0 } },
 	{ MODKEY,                       XK_comma,  focusmon,       {.i = -1 } },
